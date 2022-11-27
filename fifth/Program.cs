082e5fd989f1a29e5441af847dab06ec51380f82
thenPie <118397975+thenPie@users.noreply.github.com>
2022-11-27 16:17:32 +0300

    5th c#

---
+++ fifth/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") {
+    Console.Write("Ура, это же Маша!");
+}
+else {
+    Console.Write("Привет, " + username);
+}
