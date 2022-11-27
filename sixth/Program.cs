6d37f93242c1ad7e7d6b12f9cf889eb7022d94ff
thenPie <118397975+thenPie@users.noreply.github.com>
2022-11-27 16:25:51 +0300

    6th c#

---
+++ sixth/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (max < a)
+    max = a;
+if (max < b)
+    max = b;
+if (max < c)
+    max = c;
+if (max < d)
+    max = d;
+if (max < e)
+    max = e;
+
+Console.WriteLine(max);
